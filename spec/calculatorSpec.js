8e1f392a0874f06ce588b6163d31b3df889e94d7
Juel B <juel.braganza97@gmail.com>
2021-03-11 20:40:14 +0000

    define & test function add

---
+++ spec/calculatorSpec.js
@@ -0,0 +1,7 @@
+describe('Calculator', function() {
+    describe('adding numbers', function() {
+      it('returns the sum of the numbers', function() {
+        expect(add(3,5)).toEqual(8);
+      });
+    });
+  });
