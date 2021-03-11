e32cf877f8edd068ea8db9da7b29481e28f5200e
Juel B <juel.braganza97@gmail.com>
2021-03-11 19:52:45 +0000

    define & test function convertCelsiustoFahrenheit

--- spec/ConverterSpec.js
+++ spec/ConverterSpec.js
@@ -1,10 +1,19 @@
 describe("Cooking Ingredients Converter", function() {
 
   describe("Temperatures", function() {
-    it("Fahrenheit to Celsius", function() {
+    it("converts Fahrenheit to Celsius to 1 decimal point", function() {
       var result = convertFahrenheitToCelsius(100);
-
       expect(result).toEqual('37.8');
+
+      var result2 = convertFahrenheitToCelsius(78);
+      expect(result2).toEqual('25.6');
+    });
+    it("converts Celsius to Farenheit to 1 decimal point", function() {
+      var result = convertCelsiustoFahrenheit(37.8);
+      expect(result).toEqual('100.0');
+
+      var result2 = convertCelsiustoFahrenheit(25.6);
+      expect(result2).toEqual('78.1');
     });
   });
 });
--- src/Converter.js
+++ src/Converter.js
@@ -1,6 +1,11 @@
 /** Unit Converter **/
 function convertFahrenheitToCelsius(temperature) {
   //conversion code
-  celsius = (temperature - 32)/1.8
+  let celsius = (temperature - 32)/1.8
   return celsius.toFixed(1)
+}
+
+function convertCelsiustoFahrenheit(temperature) {
+  const fahrenheit = temperature * 1.8 + 32
+  return fahrenheit.toFixed(1)
 }
