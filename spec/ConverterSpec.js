e107378420f81b36411bdaad3d5da279d572adf5
Juel B <juel.braganza97@gmail.com>
2021-03-11 20:07:37 +0000

    change variable declaration to const

--- spec/ConverterSpec.js
+++ spec/ConverterSpec.js
@@ -2,31 +2,31 @@ describe("Cooking Ingredients Converter", function() {
 
   describe("Temperatures", function() {
     it("converts Fahrenheit to Celsius to 1 decimal point", function() {
-      var result = convertFahrenheitToCelsius(100);
+      const result = convertFahrenheitToCelsius(100);
       expect(result).toEqual('37.8');
 
-      var result2 = convertFahrenheitToCelsius(78);
+      const result2 = convertFahrenheitToCelsius(78);
       expect(result2).toEqual('25.6');
     });
     it("converts Celsius to Farenheit to 1 decimal point", function() {
-      var result = convertCelsiustoFahrenheit(37.8);
+      const result = convertCelsiustoFahrenheit(37.8);
       expect(result).toEqual('100.0');
 
-      var result2 = convertCelsiustoFahrenheit(25.6);
+      const result2 = convertCelsiustoFahrenheit(25.6);
       expect(result2).toEqual('78.1');
     });
   });
 
   describe("Weight", function() {
     it("converts Pounds to kilos to 2 decimal points", function(){
-      var kilo = convertPoundstoKilos(10);
+      const kilo = convertPoundstoKilos(10);
       expect(kilo).toEqual('4.54')
     } )
   });
 
   describe("Volume", function() {
     it("converts Litre to Gallons to 2 decimal points", function() {
-      var gallon = convertLitrestoGallons(5.1);
+      const gallon = convertLitrestoGallons(5.1);
       expect(gallon).toEqual('1.12')
     })
   })
--- src/Converter.js
+++ src/Converter.js
@@ -1,7 +1,7 @@
 /** Unit Converter **/
 function convertFahrenheitToCelsius(temperature) {
   //conversion code
-  let celsius = (temperature - 32)/1.8
+  const celsius = (temperature - 32)/1.8
   return celsius.toFixed(1)
 }
 
