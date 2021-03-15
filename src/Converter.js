967cbfaa355b55cf9e812ec7f84f009665ba8910
Juel B <juel.braganza97@gmail.com>
2021-03-15 18:06:40 +0000

    complete script + refactor

--- src/Converter.js
+++ src/Converter.js
@@ -1,26 +1,44 @@
-/** Unit Converter **/
-function convertFahrenheitToCelsius(temperature) {
-  //conversion code
-  const celsius = (temperature - 32)/1.8
-  return celsius.toFixed(1)
-}
+// select elements
+const input = document.querySelector('#input')
+const output = document.querySelector('#output')
+const inputUnit = document.querySelector('#inputUnit')
 
-function convertCelsiustoFahrenheit(temperature) {
-  const fahrenheit = temperature * 1.8 + 32
-  return fahrenheit.toFixed(1)
-}
+// event listeners
+input.addEventListener('keyup', unitConverter);
+inputUnit.addEventListener('change', unitConverter);
 
-function convertPoundstoKilos(weight) {
-  const kilo = weight * 0.4536
-  return kilo.toFixed(2)
-}
+// default output value
+output.value = '0.00'
 
-function convertLitrestoGallons(volume) {
-  const gallon = volume * 0.22
-  return gallon.toFixed(2)
-}
+// function
+function unitConverter() {
 
-function convertMilestoKm(distance) {
-  const km = distance * 1.609
-  return km.toFixed(2)
+  // get input value from user
+  let inputUnitValue = inputUnit.value;
+
+  // convert unit based on selected unit options
+  if (inputUnitValue === "Fahrenheit to Celcius" && input.value !== "") { // also check that input field is not empty (!impt)
+    let celcius = (input.value - 32) / 1.8;
+    output.value = celcius.toFixed(1);
+  }
+  
+  else if (inputUnitValue === "Miles to Kilometers") {
+    let km = input.value * 1.609;
+    output.value = km.toFixed(2);
+  }
+
+  else if (inputUnitValue === "Pounds to Kilograms") {
+    let kg = input.value * 0.4536;
+    output.value = kg.toFixed(2);
+  }
+  
+  else if (inputUnitValue === "Gallons to Litres") {
+    let gallon = input.value * 0.22;
+    output.value = gallon.toFixed(2);
+  }
+
+  else {
+    output.value = '0.00'
+  }
+
 }
