f977f0aa62bdb6f615761701fbcd8157799ecb9b
Juel B <juel.braganza97@gmail.com>
2021-03-15 00:02:10 +0000

    define & test function subtract

--- spec/calculatorSpec.js
+++ spec/calculatorSpec.js
@@ -1,14 +1,14 @@
-describe('Calculator', function() {
-    describe('adding numbers', function() {
-      it('returns the sum of the numbers', function() {
-        expect(add(3,5)).toEqual(8);
-      });
+describe('Calculator', function () {
+  describe('adding numbers', function () {
+    it('returns the sum of the numbers', function () {
+      expect(add(3, 5)).toEqual(8);
     });
   });
-describe('Calculator', function() {
-    describe('subtracting numbers', function() {
-      it('returns the difference of the numbers', function() {
-        expect(add(3,5)).toEqual(2);
-      });
+});
+describe('Calculator', function () {
+  describe('subtracting numbers', function () {
+    it('returns the difference of the numbers', function () {
+      expect(subtract(5, 3)).toEqual(2);
     });
   });
+});
